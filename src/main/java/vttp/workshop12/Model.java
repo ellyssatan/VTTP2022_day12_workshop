800423ece50acea15502e991de45a5836a0a9ebc
ellyssatan <ellyssa.tan1807@gmail.com>
2022-07-19 16:23:49 +0800

    added image output

---
+++ src/main/java/vttp/workshop12/Model.java
@@ -0,0 +1,5 @@
+package vttp.workshop12;
+
+public class Model {
+
+}
--- src/main/java/vttp/workshop12/WorkshopController.java
+++ src/main/java/vttp/workshop12/WorkshopController.java
@@ -1,15 +1,43 @@
 package vttp.workshop12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping(path = "/index")
 public class WorkshopController {
-    
-    @GetMapping
+
+    @RequestMapping(path = {"/index"})
     public String test() {
         return "index";
+    }
+
+    @RequestMapping(path = {"/processNumber"})
+    public String results (@RequestParam(name = "inputNumber", defaultValue = "1") Integer unit, Model model) {
+
+        List<Integer> lstInt = new ArrayList<>();
+        
+        int loopValue = Integer.parseInt(unit.toString());
+        int loop = 1;
+        while (loop <= loopValue) {
+
+            int results = (int) ((Math.random() * loopValue) + 1);
+
+            if (!lstInt.contains(Integer.valueOf(results))) {
+                lstInt.add(Integer.valueOf(results));
+                loop++;
+            }
+        }
+
+        for (int i = 0; i < lstInt.size(); i++) {
+            System.out.print(">>" + lstInt.get(i));
+            System.out.println();
+        }
+        model.addAttribute("numbers", lstInt);
+            return "workshopresult";
     }
 }
